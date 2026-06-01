8791851b8e16205a53d1916fd2ae2d226a12d6e5
Nicolas Tilly <underworld001@hotmail.fr>
2026-06-01 22:23:21 +0200

    update demo

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  angleMode(DEGREES);
-  noLoop();
-  translate(-width / 2, -height / 2);
-  randomSeed("v2-demo");
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    angleMode(DEGREES);
+    noLoop();
+    translate(-width / 2, -height / 2);
 
-  // Fond plat rapide avec wash
-  brush.wash("#fff8e7", 255);
-  brush.rect(0, 0, width, height);
-  brush.noWash();
+    background("#fff");
+    brush.noStroke();
+    brush.field("curved");
+    brush.wiggle();
+    brush.hatchStyle("pen", "#1c1c1d", 1);
+    brush.mass("pastel", "#e03599", { strength: 0.6, gradient: 0.3 });
+    brush.hatch(6, 45, { rand: 0.15, continuous: true });
 
-  // Remplissages massifs au pastel
-  brush.mass("pastel", "#c1502e", { strength: 0.7, gradient: 0.4 });
-  brush.circle(width * 0.3, height * 0.5, 180, true);
-
-  brush.mass("crayon", "#2a6f7c", { strength: 0.5, gradient: 0.3 });
-  brush.circle(width * 0.65, height * 0.5, 220, true);
-  brush.noMass();
-
-  // Quelques arcs au fusain par-dessus
-  brush.set("charcoal", "#1a1a1a", 1.2);
-  for (let i = 0; i < 5; i++) {
-    let r = random(80, 200);
-    brush.arc(width / 2, height / 2, r, random(0, 180), random(180, 360));
-  }
+    brush.rect(width / 2, height / 2, 300, 300, "center");
 }
 
 function draw() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  redraw();
+    resizeCanvas(windowWidth, windowHeight);
+    redraw();
 }
